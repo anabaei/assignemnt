da3ac57cdb38fb041629070b78da9664b6434893
Amir Nabaei <amirnabaei@Amirs-MacBook-Air.local>
2017-08-23 02:09:45 -0700

    add blog

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,58 +10,58 @@ Post.destroy_all
 Category.destroy_all
 User.destroy_all
 
-5.times do
-  name =  Faker::Address.state
-  Category.create(
-    name: name,
-    # email: "#{first_name.downcase}-#{last_name.downcase}@example.com",
-    # password: PASSWORD
-  )
-end
-
-5.times do
-  email = "amir@gmail.com"
-  password = "1234"
-
-  User.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.first_name,
-    email: email,
-    password_digest: password
-    # email: "#{first_name.downcase}-#{last_name.downcase}@example.com",
-    # password: PASSWORD
-  )
-end
-
-categories = Category.all
-users = User.all
-
-20.times do
-  body =  Faker::Lorem.paragraphs(rand(2..2)).join(' ')
-  title = Faker::Book.title
-  Post.create(
-    body: body,
-    title: title,
-    user: users.sample,
-    category: categories.sample
-    # email: "#{first_name.downcase}-#{last_name.downcase}@example.com",
-    # password: PASSWORD
-  )
-end
-posts = Post.all
-
-10.times do
-  comment = Comment.create(
-    title: Faker::ChuckNorris.fact,
-    body: Faker::Lorem.paragraphs(rand(2..3)).join(' '),
-    post: posts.sample,
-    user: users.sample
-  )
-end
-
-
-comments = Comment.all
-
-  puts Cowsay.say("Created #{comments.count} comments", :tux)
-  puts Cowsay.say("Created #{users.count} users", :tux)
+# 5.times do
+#   name =  Faker::Address.state
+#   Category.create(
+#     name: name,
+#     # email: "#{first_name.downcase}-#{last_name.downcase}@example.com",
+#     # password: PASSWORD
+#   )
+# end
+#
+# 5.times do
+#   email = "amir@gmail.com"
+#   password = "1234"
+#
+#   User.create(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.first_name,
+#     email: email,
+#     password_digest: password
+#     # email: "#{first_name.downcase}-#{last_name.downcase}@example.com",
+#     # password: PASSWORD
+#   )
+# end
+#
+# categories = Category.all
+# users = User.all
+#
+# 20.times do
+#   body =  Faker::Lorem.paragraphs(rand(2..2)).join(' ')
+#   title = Faker::Book.title
+#   Post.create(
+#     body: body,
+#     title: title,
+#     user: users.sample,
+#     category: categories.sample
+#     # email: "#{first_name.downcase}-#{last_name.downcase}@example.com",
+#     # password: PASSWORD
+#   )
+# end
+# posts = Post.all
+#
+# 10.times do
+#   comment = Comment.create(
+#     title: Faker::ChuckNorris.fact,
+#     body: Faker::Lorem.paragraphs(rand(2..3)).join(' '),
+#     post: posts.sample,
+#     user: users.sample
+#   )
+# end
+#
+#
+# comments = Comment.all
+#
+#   puts Cowsay.say("Created #{comments.count} comments", :tux)
+#   puts Cowsay.say("Created #{users.count} users", :tux)
   puts Cowsay.say("Created #{posts.count} posts", :tux)
