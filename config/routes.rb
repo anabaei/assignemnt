2bb7c31dba4b5c8114665f35eb6f3d9a51c4bded
Amir Nabaei <amirnabaei@Amirs-MacBook-Air.local>
2017-08-23 09:09:29 -0700

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-   get('/', { to: 'welcome#index'})
+  # get('/', { to: 'welcome#index'})
   # root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get('/', { to: 'posts#index', as: :home })
+   get('/', { to: 'posts#index', as: :home })
 
   #  resources :sessions, only: [:new, :create]
   #  resources :users, only: [:new, :show, :create]
-  #  resources :posts , only: [:index, :new, :show ,:create] do
-  #      resources :comments, only: [:index, :new, :create]
+    resources :posts , only: [:index, :new, :show ,:create]
+  #   do   resources :comments, only: [:index, :new, :create]
   #  end
 
 
