a6a4aa4ad8d4935d8bf66fa36094b8f9ab3d8e3e
Amir Nabaei <amirnabaei@Amirs-MacBook-Air.local>
2017-08-23 12:54:06 -0700

    add

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
     user = User.find_by(email: params[:session][:email])
-
      if user #&.authenticate(params[:session][:password])
        session[:user_id] = user.id
        redirect_to home_path, notice: 'Thank you for signing in! ❤️'
@@ -18,5 +16,5 @@ class SessionsController < ApplicationController
 
   def destroy
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    get('/', { to: 'posts#index', as: :home })
 
-  #  resources :sessions, only: [:new, :create]
+     resources :sessions, only: [:new, :create]
      resources :users, only: [:new, :show, :create]
      resources :posts , only: [:index, :new, :show ,:create] do
          resources :comments, only: [:index, :new, :create]
